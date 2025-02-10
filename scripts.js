9bf3a1bee9aed71ec292604b9a52b0b4b05ef459
ArtyomHloba <artemgloba2@gmail.com>
2025-02-10 22:10:26 +0200

    fix some ditails

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
-    
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        alert('Сообщение отправлено!');
-        form.reset();
-    });
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     const form = document.querySelector('form');
+
+//     form.addEventListener('submit', (e) => {
+//         e.preventDefault();
+//         alert('Сообщение отправлено!');
+//         form.reset();
+//     });
+// });
